878fa0ccf3d9517642d7522a6bb2e16b812ed151
Kye <kye@apacmediasolutions.com>
2023-10-07 14:50:38 -0400

    clean up

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 [![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)
 
-# Paper-Implementation-Template
-A simple reproducible template to implement AI research papers 
+# RT-X
+Pytorch implementation of the models RT-1-X and RT-2-X from the paper: "Open X-Embodiment: Robotic Learning Datasets and RT-X Models"
 
-Paper Link
+[Paper Link](https://robotics-transformer-x.github.io/)
 
 # Appreciation
 * Lucidrains
 * Agorians
 
-
-
 # Install
+`pip install rtx-torch `
 
 # Usage
+```python
+
+
+
+```
 
 # Architecture
 
--- rtx/film.py
+++ rtx/film.py
@@ -1,21 +1,13 @@
-import torch 
+import torch
 from torch import nn
 from PIL import Image
 
 
 class Film(nn.Module):
-    def __init__(
-        self,
-        num_channels
-    ):
+    def __init__(self, num_channels):
         super(Film, self).__init__()
-        self.gamma = nn.Parameter(
-            torch.ones(1, num_channels, 1, 1)
-        )
-        self.beta = nn.Parameter(
-            torch.zeros(1, num_channels, 1, 1)
-        )
-    
+        self.gamma = nn.Parameter(torch.ones(1, num_channels, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(1, num_channels, 1, 1))
+
     def forward(self, x):
         return x * self.gamma * self.beta
-    
