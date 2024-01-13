8057f7d805a693aa777ee3bd5ee61b095fe82533
Sebastian Peralta <peraltas@seas.upenn.edu>
2024-01-13 12:55:27 +0000

    use absl and refactor

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from examples import rtx1_example, rtx2_example, train_example
 from rtx import RTX1, RTX2
 from rtx.rtx1 import FilmViTConfig
-from absl import app, flags
+from absl import app, flags, logging
+import sys
 
 REGISTRY = {
     "rtx1": RTX1,
@@ -24,6 +25,8 @@ def main(_):
     if FLAGS.mode == "inference":
         EXAMPLE_SCRIPTS[FLAGS.model].run()
     elif FLAGS.mode == "train":
+        if FLAGS.pretrained_vit and FLAGS.model == "rtx2":
+            logging.fatal("Option `pretrained_vit` is not available for model {} ".format(FLAGS.model))
         model = REGISTRY[FLAGS.model](vit_config= FilmViTConfig(pretrained= FLAGS.pretrained_vit))
         train_example.run(model)
 
